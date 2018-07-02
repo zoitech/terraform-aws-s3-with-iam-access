95239e272f7e0dd6e4cf0b19caf1033901595b1b
ZOI\graham.geary <gg@zoi.de>
2018-07-02 10:41:45 +0200

    Removed "enable" variable for user creation

--- variables_iam_users.tf
+++ variables_iam_users.tf
@@ -1,8 +1,3 @@
-variable "iam_user_s3_standard" {
-  description = "Enable IAM user(s) with S3 bucket full access"
-  default     = false
-}
-
 variable "iam_user_s3_standard_names" {
   type        = "list"
   description = "Names of the IAM users with standard access"
