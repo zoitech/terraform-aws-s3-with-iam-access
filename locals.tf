b768591308ff3bca441169ab4a4325b8fb3ce9f1
ZOI\graham.geary <gg@zoi.de>
2018-07-02 10:40:48 +0200

    Changed IAM user creation to be based on when the lists have values, as opposed to an "enable" variable

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   #IAM static users 
-  count_iam_user_s3_full_names        = "${var.iam_user_s3_full ? "${length(var.iam_user_s3_full_names)}" :0 }"
-  count_iam_user_s3_list_delete_names = "${var.iam_user_s3_list_delete ? "${length(var.iam_user_s3_list_delete_names)}" :0 }"
-  count_iam_user_s3_get_delete_names  = "${var.iam_user_s3_get_delete ? "${length(var.iam_user_s3_get_delete_names)}" :0 }"
+  count_iam_user_s3_full_names        = "${length(var.iam_user_s3_full_names)}" 
+  count_iam_user_s3_list_delete_names = "${length(var.iam_user_s3_list_delete_names)}"
+  count_iam_user_s3_get_delete_names  = "${length(var.iam_user_s3_get_delete_names)}"
 
   #IAM standard users
   count_standard_user = "${length(var.iam_user_s3_standard_names)}"
