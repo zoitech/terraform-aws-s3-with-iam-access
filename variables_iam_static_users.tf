c2cb0d31c5765dc1c47a0e6e41aec02ac090f2a0
ZOI\graham.geary <gg@zoi.de>
2018-07-02 10:41:15 +0200

    Removed "enable" variables for IAM user creation

--- variables_iam_static_users.tf
+++ variables_iam_static_users.tf
@@ -1,7 +1,3 @@
-variable "iam_user_s3_full" {
-  description = "Enable IAM user(s) with S3 bucket full access"
-  default     = false
-}
 
 variable "iam_user_s3_full_names" {
   type        = "list"
@@ -9,20 +5,10 @@ variable "iam_user_s3_full_names" {
   default     = []
 }
 
-variable "iam_user_s3_list_delete" {
-  description = "Enable IAM user(s) with S3 bucket list/delete permissions"
-  default     = false
-}
-
 variable "iam_user_s3_list_delete_names" {
   type        = "list"
   description = "Names of the IAM users with S3 bucket list/delete permissions"
   default     = []
-}
-
-variable "iam_user_s3_get_delete" {
-  description = "Enable IAM user(s) with S3 bucket get/delete permissions"
-  default     = false
 }
 
 variable "iam_user_s3_get_delete_names" {
